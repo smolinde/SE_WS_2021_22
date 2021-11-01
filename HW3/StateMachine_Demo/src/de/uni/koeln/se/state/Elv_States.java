47ad8cb41e2666ff5faad7e7b997f4cba657712d
smolinde <denis.smolin@gmx.de>
2021-11-01 13:18:34 +0100

    Added Solution Of Task One
    	+ new uxf file, can be opened with UMLet

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
@@ -4,5 +4,5 @@ public class Elv_States {
 	public final String Moving_up = "Moving_up";
 	public final String Moving_down = "Moving_down";
 	public final String Idle = "Idle";
-	public  String Current_State = Idle;
+	public String Current_State = Idle;
 }
